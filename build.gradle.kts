d8ef3f1f0e44768683b09d23cdd12bcdf81f008a
v79 <liamjdavison@gmail.com>
2020-12-02 21:18:05 +0000

    Spotify stuff basically working now. Started to use the kotlinx-datetime library for date-time stuff.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	kotlin("multiplatform") version "1.4.20"
+	kotlin("plugin.serialization") version "1.4.10"
 }
 
 group = "org.liamjd.pi"
@@ -8,15 +9,13 @@ version = "1.1-SNAPSHOT"
 repositories {
 	mavenCentral()
 	jcenter()
+	maven(url = "https://kotlin.bintray.com/kotlinx/")
+	mavenLocal()
 }
-
 
 kotlin {
 	val hostOs = System.getProperty("os.name")
 	val isMingwX64 = hostOs.startsWith("Windows")
-
-
-
 
 	linuxArm32Hfp("Pi") {
 
@@ -47,6 +46,8 @@ kotlin {
 		val PiMain by getting {
 			dependencies {
 				implementation("org.jetbrains.kotlinx:kotlinx-coroutines-io:0.1.16")
+				implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+				implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0-SNAPSHOT")
 			}
 		}
 	}
